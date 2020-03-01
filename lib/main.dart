98a55825c14f7fba29749b75fd9af8cb73a01f9e
dev-marceloborba <marceloborba@MacBook-Pro-de-Quantico.local>
2020-03-01 01:21:40 -0300

    adjusting widgets to work with landscape mode

--- lib/main.dart
+++ lib/main.dart
@@ -71,6 +71,8 @@ class _MyHomePageState extends State<MyHomePage> {
         });
   }
 
+  bool _showChart = false;
+
   List<Transaction> get _recentTransactions {
     return _userTransactions
         .where((element) =>
@@ -102,12 +104,29 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Show Chart'),
+                Switch(
+                  value: _showChart,
+                  onChanged: (val) {
+                    setState(() {
+                      _showChart = val;
+                    });
+                  },
+                )
+              ],
+            ),
+            _showChart
+            ?
             Container(
                 height: (MediaQuery.of(context).size.height -
                         appBar.preferredSize.height -
                         MediaQuery.of(context).padding.top) *
                     0.3,
-                child: Chart(_recentTransactions)),
+                child: Chart(_recentTransactions))
+            :
             Container(
                 height: (MediaQuery.of(context).size.height -
                         appBar.preferredSize.height -
